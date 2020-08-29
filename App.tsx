d108e0d3c1c191e9fba59b81a0e146d73a50075b
DhawanRachakonda <r.dhawan27@hotmail.co.uk>
2020-08-29 17:22:03 +0530

    feat: styles excercise

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { ReactNode } from 'react';
+import { Text, StyleSheet, SafeAreaView, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+interface IAppViewProps {
+  children: ReactNode;
+  styleObject: any;
 }
 
+const AppView = ({ children, styleObject }: IAppViewProps) => {
+  return <Text style={[styles.containerText, styleObject]}>{children}</Text>;
+};
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <AppView styleObject={{ ...styles.bold }}>
+        Here are some basics of different colors
+      </AppView>
+      <AppView styleObject={{ ...styles.cyan, ...styles.center }}>Cyan</AppView>
+      <AppView styleObject={{ ...styles.blue, ...styles.center }}>Blue</AppView>
+      <AppView styleObject={{ ...styles.magenta, ...styles.center }}>
+        Magenta
+      </AppView>
+      <AppView styleObject={{ ...styles.orange, ...styles.center }}>
+        Orange
+      </AppView>
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerText: {
+    padding: 10,
+    marginBottom: 10,
+  },
+  center: {
+    textAlign: 'center',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  cyan: {
+    backgroundColor: '#008B8B',
+  },
+  blue: {
+    backgroundColor: '#1E90FF',
+  },
+  magenta: {
+    backgroundColor: '#FF00FF',
+  },
+  orange: {
+    backgroundColor: '#FFA500',
   },
 });
+
+export default App;
